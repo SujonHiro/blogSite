afdb18fcfd09841a1fc98a9b02aa9a7634b58a45
Sujon <57975469+SujonHiro@users.noreply.github.com>
2023-10-18 09:01:36 +0600

    [DONE]- Create User

--- blog-Backend/src/moduels/users/user.services.js
+++ blog-Backend/src/moduels/users/user.services.js
@@ -1,6 +1,10 @@
 import userModel from "./user.model.js"
 
 const userCreateService=async(userData)=>{
+    const exitingUser=await userModel.findOne({email: userData.email});
+    if(exitingUser){
+        throw new Error("Email already exist");
+    }
     const user=await userModel.create(userData);
     return user;
 }
